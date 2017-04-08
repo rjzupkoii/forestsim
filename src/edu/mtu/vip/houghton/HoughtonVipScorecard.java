dcda7e7e390809e8b0cc27bdea6c63b06fa9827b
rjzupkoii <rjzii@yahoo.com>
2017-04-08 17:31:37 -0400

    Updates to the POC model.

--- src/edu/mtu/vip/houghton/HoughtonVipScorecard.java
+++ src/edu/mtu/vip/houghton/HoughtonVipScorecard.java
@@ -1,6 +1,5 @@
 package edu.mtu.vip.houghton;
 
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -8,71 +7,102 @@ import java.io.IOException;
 import edu.mtu.environment.Forest;
 import edu.mtu.simulation.Scorecard;
 import edu.mtu.steppables.marketplace.AggregateHarvester;
-import sim.field.geo.GeomGridField;
-import sim.io.geo.ArcInfoASCGridExporter;
 
 public class HoughtonVipScorecard implements Scorecard {
-	
+
 	private final static String biomassFile = "/biomass.csv";
-	private final static String stockingFile = "/stocking%1$d.asc";
+	private final static String carbonFile = "/carbon.csv";
+	private final static String recreationFile = "/recreation.csv";
 	private final static String vipFile = "/vip.csv";
-	
-	private static int step = 0;
-	private static int nextExport = 10;
-	
+
 	private String outputDirectory;
-	
+
 	public HoughtonVipScorecard(String directory) {
 		outputDirectory = directory;
 	}
-	
+
 	@Override
 	public void processTimeStep() {
 		try {
-			// Collect the VIP membership
-			VIP vip = VIP.getInstance();
-			FileWriter writer = new FileWriter(outputDirectory + vipFile, true);
-			writer.write(vip.getSubscriptionRate() + "," + vip.getSubscribedArea() + ",");
-			writer.write(System.lineSeparator());
-			writer.close();
-					
-			// Collect the biomass harvested
-			double biomass = AggregateHarvester.getInstance().getBiomass();
-			writer = new FileWriter(outputDirectory + biomassFile, true);
-			writer.write(biomass + ",");
-			writer.write(System.lineSeparator());
-			writer.close();
-			
-			// Check the step and export as needed
-			step++;
-			if (step == nextExport) {
-				GeomGridField stocking = Forest.getInstance().getStocking();
-				BufferedWriter output = new BufferedWriter(new FileWriter(String.format(outputDirectory + stockingFile, step)));
-				ArcInfoASCGridExporter.write(stocking, output);
-				output.close();
-				nextExport += 10;
-			}
+			writeCarbonSequestration();
+			writeHarvestedBiomass();
+			writeRecreationalAccess();
 		} catch (IOException ex) {
 			System.err.println("Unhandled IOException: " + ex.toString());
 			System.exit(-1);
-		}		
+		}
 	}
 
 	@Override
 	public void processInitialization() {
+		File directory = new File(outputDirectory);
+		directory.mkdirs();
+	}
+
+	@Override
+	public void processFinalization() {
 		try {
-			// Bootstrap any relevant paths
-			File directory = new File(outputDirectory);
-			directory.mkdirs();
-						
-			// Store the initial stocking
-			BufferedWriter output = new BufferedWriter(new FileWriter(outputDirectory + "/stocking0.asc"));
-			GeomGridField stocking = Forest.getInstance().getStocking();
-			ArcInfoASCGridExporter.write(stocking, output);
-			output.close();		
+			FileWriter writer = new FileWriter(outputDirectory + biomassFile, true);
+			writer.write(System.lineSeparator());
+			writer.close();
+			
+			writer = new FileWriter(outputDirectory + carbonFile, true);
+			writer.write(System.lineSeparator());
+			writer.close();
+			
+			writer = new FileWriter(outputDirectory + recreationFile, true);
+			writer.write(System.lineSeparator());
+			writer.close();
+			
+			writer = new FileWriter(outputDirectory + vipFile, true);
+			writer.write(System.lineSeparator());
+			writer.close();
 		} catch (IOException ex) {
 			System.err.println("Unhandled IOException: " + ex.toString());
 			System.exit(-1);
-		}	
+		}
+	}
+	
+	/**
+	 * Find the approximate amount of carbon in the woody biomass.
+	 * 
+	 * @param biomass
+	 *            The woody biomass in green tons.
+	 * @return The carbon content of the vegetation in gigatons (Gt)
+	 */
+	private double carbonInBiomassEstiamte(double biomass) {
+		// Use the approximation given by (Magnussen & Reed, 2015)
+		// 62% moisture content is a rough approximation from (Wenger 1984)
+		return (0.475 * (biomass * 0.62)) / 1000000000;
+	}
+
+	// Society: VIP Enrollment, Recreational Access
+	private void writeRecreationalAccess() throws IOException {
+		VIP vip = VIP.getInstance();
+		FileWriter writer = new FileWriter(outputDirectory + recreationFile, true);
+		writer.write(vip.getSubscribedArea() + ",");
+		writer.close();
+		
+		writer = new FileWriter(outputDirectory + vipFile, true);
+		writer.write(vip.getSubscriptionRate() + ",");
+		writer.close();
+	}
+
+	// Environment: Carbon Sequestration
+	private void writeCarbonSequestration() throws IOException {
+		double biomass = Forest.getInstance().calculateTotalBiomass();
+		double carbon = carbonInBiomassEstiamte(biomass);
+		FileWriter writer = new FileWriter(outputDirectory + carbonFile, true);
+		writer.write(carbon + ",");
+		writer.close();
+	}
+
+	// Economic: Woody Biomass Availability, Reliability / consistent supply of
+	// woody biomass
+	private void writeHarvestedBiomass() throws IOException {
+		double biomass = AggregateHarvester.getInstance().getBiomass();
+		FileWriter writer = new FileWriter(outputDirectory + biomassFile, true);
+		writer.write(biomass + ",");
+		writer.close();
 	}
 }
